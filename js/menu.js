b77753c7fc908690a8211ac0d8170fc0d273f771
joaopedro0017 <joaopedrodiasandrade8@gmail.com>
2024-12-03 20:41:29 -0300

    Algumas Atualizacoes

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,14 @@
-let jpMenu = document.getElementById('jp-menu');
-let menu = document.getElementById('menu-mobile');
+let jpMenu = document.getElementById('jp-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+jpMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
